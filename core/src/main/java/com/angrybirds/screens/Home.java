baf6e101e825fe48945939c2e0744c3e25e52e75
ridhhingrover <ridhhin2027@gmail.com>
2024-10-17 23:18:09 +0530

    Image +file

---
+++ core/src/main/java/com/angrybirds/screens/Home.java
@@ -0,0 +1,55 @@
+package com.angrybirds.screens;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.angrybirds.Main;
+
+public class Home implements Screen{
+    private Texture texture;
+    private Playscreen game;
+    public Home(Playscreen game){
+        this.game=game;
+        texture= new Texture("img.png");
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
