69d2a88bf2e3f29646f19da4e250853a4f948c5f
shivam <shivamois8@gmail.com>
2024-10-22 00:17:54 +0530

    fix

---
+++ core/src/main/java/com/angrybirds/screens/levels/levelthree.java
@@ -0,0 +1,36 @@
+package com.angrybirds.screens.levels;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class levelthree implements ApplicationListener
+{
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
---
+++ core/src/main/java/com/angrybirds/screens/levels/leveltwo.java
@@ -0,0 +1,35 @@
+package com.angrybirds.screens.levels;
+
+import com.badlogic.gdx.ApplicationListener;
+
+public class leveltwo implements ApplicationListener {
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
